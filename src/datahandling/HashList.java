93b8851ed70d316daa26b1a194f3c7d8fd6d1ea1
Deyan2306 <deyan.sirakov2006@abv.bg>
2023-11-06 22:40:40 +0200

    HashList implementation

--- src/datahandling/HashList.java
+++ src/datahandling/HashList.java
@@ -1,4 +1,31 @@
 package datahandling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HashList<T> {
+
+    private List<T> hashes;
+
+    public HashList() {
+        this.hashes = new ArrayList<T>();
+    }
+
+    public int getAllHashesCount() {
+        return this.hashes.size();
+    }
+
+    public boolean addHash(T hash) {
+        return this.hashes.add(hash);
+    }
+
+    public boolean removeHash(T hash) {
+        return this.hashes.remove(hash);
+    }
+
+    public boolean removeHashAt(int index) {
+        T removed = this.hashes.remove(index);
+        return null != removed;
+    }
+
 }
