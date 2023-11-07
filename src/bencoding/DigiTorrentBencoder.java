0e705877d63d0d51c6f7632e46618cec1d5446ea
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-07 08:44:58 +0200

    Some implementation for the bencoder

--- src/bencoding/DigiTorrentBencoder.java
+++ src/bencoding/DigiTorrentBencoder.java
@@ -3,37 +3,109 @@ package bencoding;
 import exceptions.InvalidTorrentFileException;
 import exceptions.UnsupportedBencodeValue;
 
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class DigiTorrentBencoder {
 
-    public static void main(String[] args) throws IOException, InvalidTorrentFileException {
-        String bencodedFile = bencodeFileToString(new File("/home/deyan/Documents/Coding/DigiTorrent/src/temp/sinkhole.torrent"));
+    // Pretty much static atp
+    private final static String TEMP_FILE_URI = "/home/deyan/Documents/Coding/DigiTorrent/src/temp/sinkhole.torrent";
 
-        System.out.println((char) bencodedFile.charAt(5));
+    private static boolean integerFlag = false;
+    private static boolean byteFlag = false;
+    private static boolean listFlag = false;
+    private static boolean dictionaryFlag = false;
+
+
+    public static void main(String[] args) throws IOException, InvalidTorrentFileException, UnsupportedBencodeValue {
+        String bencodedFile = bencodeFileToString(new File(TEMP_FILE_URI));
+        Object bencodedData = decode(bencodedFile);
+
+        System.out.println(bencodedData);
     }
 
-    public static void handleBencodedString(String string) throws UnsupportedBencodeValue {
-        char[] sequenceData = string.toCharArray();
-
-        for (int index = 0; index < sequenceData.length; index++) {
-
-            if (sequenceData[index] == 'i') { // Most probably we have a integer
-
-            } else if (Character.isDigit(sequenceData[index])) { // Most probably we have a byte
-
-            } else if (sequenceData[index] == 'l') { // Most probably we have a list
-
-            } else if (sequenceData[index] == 'd') { // Most probably we have a dictionary
-
-            } else {
-                throw new UnsupportedBencodeValue("This character is not indicating anything");
-            }
-
+    public static Object decode(byte[] data) {
+        try {
+            return decode(new String(data, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
         }
-
     }
+
+    public static Object decode(String s) {
+        char[] chars = s.toCharArray();
+        int index = 0;
+        return decodeValue(chars, index);
+    }
+
+    private static Object decodeValue(char[] chars, int index) {
+        char c = chars[index];
+        if (Character.isDigit(c)) {
+            return decodeString(chars, index);
+        } else if (c == 'i') {
+            return decodeInteger(chars, index);
+        } else if (c == 'l') {
+            return decodeList(chars, index);
+        } else if (c == 'd') {
+            return decodeDictionary(chars, index);
+        }
+        return null;
+    }
+
+    private static String decodeString(char[] chars, int index) {
+        int length = 0;
+        while (Character.isDigit(chars[index])) {
+            length = length * 10 + (chars[index] - '0');
+            index++;
+        }
+        if (chars[index] != ':') {
+            return null;
+        }
+        index++;
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            str.append(chars[index + i]);
+        }
+        return str.toString();
+    }
+
+    private static Long decodeInteger(char[] chars, int index) {
+        index++; // Skip 'i'
+        int endIndex = index;
+        while (chars[endIndex] != 'e') {
+            endIndex++;
+        }
+        String intStr = new String(chars, index, endIndex - index);
+        return Long.parseLong(intStr);
+    }
+
+    private static List<Object> decodeList(char[] chars, int index) {
+        index++; // Skip 'l'
+        List<Object> list = new ArrayList<>();
+        while (chars[index] != 'e') {
+            Object value = decodeValue(chars, index);
+            list.add(value);
+            index += value.toString().length();
+        }
+        return list;
+    }
+
+    private static Map<String, Object> decodeDictionary(char[] chars, int index) {
+        index++; // Skip 'd'
+        Map<String, Object> dict = new LinkedHashMap<>();
+        while (chars[index] != 'e') {
+            String key = decodeString(chars, index);
+            index += key.length() + 2; // +2 to skip ':'
+            Object value = decodeValue(chars, index);
+            dict.put(key, value);
+            index += value.toString().length();
+        }
+        return dict;
+    }
+
 
     public static String bencodeFileToString(File file) throws IOException, InvalidTorrentFileException {
         FileInputStream stream = new FileInputStream(file);
@@ -51,14 +123,6 @@ public class DigiTorrentBencoder {
         reader.close();
 
         return torrentContent;
-    }
-
-    public static Integer bencodeInt(String sequence) {
-        return Integer.valueOf(sequence.substring(1, sequence.length() - 1));
-    }
-
-    public static char[] bencodeBytes(String sequence) {
-        return sequence.split(":")[1].toCharArray();
     }
 
 }
