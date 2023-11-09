9babdde9d32baee385a97286b1925fcfff97ebf1
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-09 09:49:18 +0200

    Some shi done and stuff

--- src/bencoding/DigiTorrentBencoder.java
+++ src/bencoding/DigiTorrentBencoder.java
@@ -11,6 +11,8 @@ public class DigiTorrentBencoder {
     // TODO: Throws some awful exception...
     private final static String TEMP_FILE_URI = "/home/deyan/Documents/Coding/DigiTorrent/src/temp/sinkhole.torrent";
     private static int index;
+    private static int ins;
+    private static Long pieceLength;
     
     public static void main(String[] args) throws IOException, InvalidTorrentFileException {
         String bencodedFile = bencodeFileToString(new File(TEMP_FILE_URI));
@@ -77,11 +79,12 @@ public class DigiTorrentBencoder {
 
     private static Long decodeInteger(char[] chars) {
         index++; // Skip 'i'
+        ins++;
         int endIndex = index;
         while (chars[endIndex] != 'e') {
             endIndex++;
         }
-
+        ins--;
         String intStr = new String(chars, index, endIndex - index);
         index = endIndex; // move the pointer to the end index
         index++; // Skip the 'e'
@@ -91,27 +94,76 @@ public class DigiTorrentBencoder {
 
     private static List<Object> decodeList(char[] chars) {
         index++; // Skip 'l'
+        ins++;
         List<Object> list = new ArrayList<>();
         while (chars[index] != 'e') {
             Object value = decodeValue(chars);
             list.add(value);
         }
+        ins--;
         index++; //skip 'e'
         return list;
     }
 
     private static Map<String, Object> decodeDictionary(char[] chars) {
         index++; // Skip 'd'
+        ins++;
         Map<String, Object> dict = new LinkedHashMap<>();
         while (chars[index] != 'e') {
 
             String key = decodeString(chars);
+            Object value;
 
-            Object value = decodeValue(chars);
+            if ("pieces".equals(key)) {
+                value = decodePieces(chars, dict);
+            } else {
+                value = decodeValue(chars);
+            }
+
             dict.put(key, value);
         }
+        ins--;
         index++; // Skip 'e'
         return dict;
+    }
+
+    private static Object decodePieces(char[] chars, Map<String, Object> dict) {
+        // Get the array length
+        int length = 0;
+        while (Character.isDigit(chars[index])) {
+            length = length * 10 + (chars[index] - '0');
+            index++;
+        }
+        if (chars[index] != ':') {
+            return null;
+        }
+        index++;
+
+        // Get a copy of the array
+        int start = index;
+        int end = chars.length - ins;
+        long pieceLength = (long) dict.get("piece length");
+        char[] extractedPieces = getPiecesString(chars, start, end);
+        int singleHashLength = 20;
+
+        List<char[]> pieces = new ArrayList<>();
+
+        for (int index = 0; index < extractedPieces.length; index += singleHashLength) {
+            char[] piecesHash = Arrays.copyOfRange(extractedPieces, index, index + singleHashLength);
+            pieces.add(piecesHash);
+        }
+
+        return pieces;
+    }
+
+    private static char[] getPiecesString(char[] chars, int start, int end) {
+        char[] extracted = new char[end - start];
+
+        for (int i = 0; i < end - start; i++) {
+            extracted[i] = chars[index + i];
+        }
+
+        return extracted;
     }
 
     public static String bencodeFileToString(File file) throws IOException, InvalidTorrentFileException {
